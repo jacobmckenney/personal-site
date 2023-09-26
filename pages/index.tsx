19a39efa665dd6742bd7fa18cc3b5064e9aceda9
Jacob McKenney <jake.g.mckenney@gmail.com>
2023-09-26 15:02:37 -0700

    more wipe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import { ReactElement } from "react";
-import Layout from "../components/layout";
 import { NextPageWithLayout } from "./_app";
 
 const Home: NextPageWithLayout = () => {
     return <>Under Construction</>;
-};
-
-Home.getLayout = (page: ReactElement) => {
-    return <Layout>{page}</Layout>;
 };
 
 export default Home;
